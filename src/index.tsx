419db56d3a35cf09722e8fc3f43037b1f8609a9b
Eugeny Architector <it-xp@pm.me>
2022-01-31 10:58:17 +0300

    scenarios refactoring

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,6 @@
 import React, { 
-  useRef, 
-  useMemo, 
-  useReducer, 
-  useContext, 
-  createContext, 
-  ReactNode,
-  Context,
-  FC,
+  useRef, useMemo, useReducer, useContext, 
+  createContext, Context, ReactNode, FC,
 } from 'react';
 
 type TActions = {
@@ -27,41 +21,51 @@ export type TReducers<TState> = {
   [action: string]: (store: TState, payload?: any) => TState;
 };
 
-export type TScenarios<TState> = 
-  (getState: () => TState, actions: TActions) => TScripts;
+export type TScenarios<TState> = {
+  [script: string]: (getState: () => TState, actions: TActions, payload: any) => void | Promise<void>;
+}
+
+export type TBulkScenarios<TState> = (getState: () => TState, actions: TActions) => TScripts;
 
 export function useDatanomy<TState>(
   initialState: TState, 
   reducers: TReducers<TState>, 
-  scenarios?: TScenarios<TState>
+  scenarios?: TScenarios<TState> | TBulkScenarios<TState>
 ): [TState, TActions, TScripts] {
   
-  function reducer(
+  const reducer = (
     store: TState, 
     {type, payload}: {type: string, payload: any}
-  ): TState {
-    return type in reducers ? reducers[type](store, payload) : store;
-  };
+  ): TState => type in reducers ? reducers[type](store, payload) : store;
   
   const [store, dispatch] = useReducer(reducer, initialState);
   
   const actions: TActions = useMemo(() => Object.keys(reducers).reduce(
-    (actions: TActions, type) => { 
-      actions[type] = (payload: any) => dispatch({type, payload}) 
-      return actions
-    }, {} as TActions
-
+    (actions: TActions, type) => ( 
+      actions[type] = (payload: any) => {dispatch({type, payload})}, 
+      actions
+    ), {} as TActions
   // eslint-disable-next-line
   ), []);
 
   const stored = useRef(store);
-  const state = useMemo(() => {
-    stored.current = store;
-    return stored;
-  }, [store]);
+  const state = useMemo(() => (stored.current = store, stored), [store]);
   
-  // eslint-disable-next-line
-  const scripts = !scenarios ? {} : useMemo(() => scenarios(() => state.current, actions), []);
+  const scripts: TScripts = !scenarios 
+    ? {}
+    : typeof scenarios === 'object'
+      ? useMemo(() => Object.keys(scenarios).reduce(
+          (scripts: TScripts, type): TScripts => (
+            scripts[type] = (payload: any) => {
+              scenarios[type](() => state.current, actions, payload)
+            },
+            scripts
+          ), {} as TScripts
+        // eslint-disable-next-line
+        ), [])
+      : useMemo(() => (scenarios as TBulkScenarios<TState>)(
+        () => state.current, actions), []
+      )
   
   return [store, actions, scripts];
 };
@@ -69,33 +73,23 @@ export function useDatanomy<TState>(
 export function createDatanomy<TState>(
   initialState: TState, 
   reducers: TReducers<TState>, 
-  scenarios?: TScenarios<TState>
+  scenarios?: TScenarios<TState> | TBulkScenarios<TState>
 ): [
   FC<IProviderProps>,
   () => TDatanomyContext<TState>,
   Context<TDatanomyContext<TState>>
-] {
-  
-  const context = createContext<TDatanomyContext<TState>>(
-    [initialState, {}, {}]
-  );
+] {  
+  const context = createContext<TDatanomyContext<TState>>([initialState, {}, {}]);
 
   const { Provider } = context;
   
-  const DatanomyProvider: FC<IProviderProps> = ({children}) => {
-    const [
-      store, actions, scripts
-    ] = useDatanomy<TState>(initialState, reducers, scenarios);
-    return (
-      <Provider value={[store, actions, scripts]}>
-          {children}
-      </Provider>
-    );
-  };
+  const DatanomyProvider: FC<IProviderProps> = ({children}) => (
+    <Provider value={useDatanomy<TState>(initialState, reducers, scenarios)}>
+        {children}
+    </Provider>
+  );
   
-  function useHook(): TDatanomyContext<TState> {
-    return useContext(context);
-  }
+  const useHook: () => TDatanomyContext<TState> = () => useContext(context);
   
   return [ DatanomyProvider, useHook, context ];
 };
